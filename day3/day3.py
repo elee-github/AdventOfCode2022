36578df99c40a2728137832f1e68f5ce5b507f74
elee-github <elee2020@yahoo.com>
2022-12-02 21:49:07 -0800

    day3

---
+++ day3/day3.py
@@ -0,0 +1,49 @@
+def part1(comp1, comp2):
+    set1 = set(comp1)
+    for chara in comp2:
+        if chara in set1:
+            if 'A' <= chara and 'Z' >= chara:
+                return ord(chara) - 65 + 27
+            elif 'a' <= chara and 'z' >= chara:
+                return ord(chara) - 96
+            else:
+                print("BAD THING")                  #should never happen
+                return -9999999999
+
+def part2(lines):
+    elf1, elf2, elf3 = lines
+    set1 = set(elf1)
+    set2 = set(elf2)
+    for chara in elf3:
+        if chara in set1 and chara in set2:
+            if 'A' <= chara and 'Z' >= chara:
+                return ord(chara) - 65 + 27
+            elif 'a' <= chara and 'z' >= chara:
+                return ord(chara) - 96
+            else:
+                print("BAD THING")                  #should never happen
+                return -9999999999
+
+part1ans = 0
+part2ans = 0
+
+with open("day3/day3input", "r") as file:
+    line = file.readline().strip()
+    lineList = []
+    lineCount = 0
+
+    while line != "EOF":
+        halfLine = len(line) // 2
+        part1ans += part1(line[:halfLine], line[halfLine:])
+
+        lineCount += 1
+        lineList.append(line)
+        if lineCount >= 3:
+            part2ans += part2(lineList)
+            lineList = []
+            lineCount = 0
+        
+        line = file.readline().strip()
+
+print(part1ans)
+print(part2ans)
